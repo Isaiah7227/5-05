1047507d0a871a388eb3139710c8dfbbbcb6fd7a
Isaiah <isaiah.mackay@ocsbstudent.ca>
2021-02-24 16:12:07 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+let number = 0
+let sentence = 'hello, this is the number 3.'
+
+document.getElementById('button').addEventListener                                  ('click', addition)
+  
+function addition() {
+  number = document.getElementById('input').value
+
+
+
+  alert(number)
+}
